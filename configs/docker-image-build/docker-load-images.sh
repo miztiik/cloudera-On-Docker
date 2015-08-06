e650b1add701b8dbad50b1b0681991d0f6bd940b
mystique <13oct08@quantumfoam.uni.me>
2015-08-06 20:48:18 +0530

    cosmetic changes.

--- configs/docker-image-build/docker-load-images.sh
+++ configs/docker-image-build/docker-load-images.sh
@@ -10,16 +10,18 @@
 
 
 IMAGE_LOCATION=/media/sf_dockerRepos/dockerBckUps
-echo "The current working directory is $IMAGE_LOCATION"
+echo -e "\n The current working directory is $IMAGE_LOCATION"
 
 cd $IMAGE_LOCATION
 ## declare the list of images to load here, use double quote to avoid breaking with whitespace characters
-declare -a imageList=("hadoopBasev2.tar" "hadoop_cloudera_mgr_base_v1.tar" "httpdBase.tar" "weavedns_1.0.1.tar" "weaveexec_1.0.1.tar" "weave_1.0.1.tar" "testing")
+# declare -a imageList=("hadoopBasev2.tar" "hadoop_cloudera_mgr_base_v1.tar" "httpdBase.tar" "weavedns_1.0.1.tar" "weaveexec_1.0.1.tar" "weave_1.0.1.tar" "testing")
+
+declare -a imageList=("mysqlbase.tar")
 
 # Function to load the images, planning to re-use it.
 function loadImages () {
-	echo "Starting to load image  : $1"
-	docker load < $1 &&	echo "COMPLETED loading image : $1"	|| echo "Loading of image : $1 FAILED. Check the logs"
+	echo -e "\nStarting to load image  : $1"
+	docker load < $1 &&	echo "COMPLETED loading image : $1"	|| echo "FAILED loading image : $1. Check the logs"
 	}
 
 ## now loop through the above array
