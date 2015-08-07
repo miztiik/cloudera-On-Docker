90e539f79b3ff0a42a633349fa3ea12ef4ec1f17
mystique <13oct08@quantumfoam.uni.me>
2015-08-07 20:04:24 +0530

    Need to integreate this with the image loader script

---
+++ configs/docker-image-build/pickImags.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+prompt="Please select a file:"
+options=( $(find -maxdepth 1 -print0 | xargs -0) )
+
+PS3="$prompt "
+select opt in "${options[@]}" "Quit" ; do 
+    if (( REPLY == 1 + ${#options[@]} )) ; then
+        exit
+
+    elif (( REPLY > 0 && REPLY <= ${#options[@]} )) ; then
+        echo  "You picked $opt which is file $REPLY"
+        break
+
+    else
+        echo "Invalid option. Try another one."
+    fi
+done    
+
+ls -ld $opt
