78a5e0d8b6e3d9b2924c20c9fb6db1d07d3a356c
mystique <13oct08@quantumfoam.uni.me>
2015-08-22 00:58:02 +0530

    Modified to remove some onscreen errors - added a check for docker running before executing anything

--- docker-dns-build/docker-weavedns-setup.sh
+++ docker-dns-build/docker-weavedns-setup.sh
@@ -3,8 +3,8 @@
 ##################################################################################
 ## DNS setup in docker using Weave, Deciced to use weave as it have built in other funcationalities that can be levarage for monitoring.
 ##
-## VERSION		:0.0.2
-## DATE			:01Aug2015
+## VERSION		:0.0.3
+## DATE			:22Aug2015
 ##				:Weave uses PORT - 6783 , Use WEAVE_PORT to override
 ## Ref[1]		:https://github.com/weaveworks/weave#installation
 ## Ref[2]		:http://docs.weave.works/weave/latest_release/weavedns.html
@@ -18,33 +18,24 @@ WEAVE_FILE=/usr/local/bin/weave
 # WEAVE_DNSHOST_IP=10.2.254.1/24
 # WEAVE_DOMAINS="navcluster.org"
 
-function launch_weave () {
-	weave launch && weave launch-dns && weave launch-proxy
-	eval $(weave proxy-env)
-	# weave launch-dns --domain='stellaverse.srv.'
+function startWeave() {
+	# Lets check if weave environment variable is set if not set it
+	if [[ -z "$DOCKER_HOST" ]] 2>&1 > /dev/null; then
+		{ weave launch &> /dev/null && weave launch-dns &> /dev/null && weave launch-proxy &> /dev/null && eval $(weave proxy-env) &> /dev/null && printf "\n\n\t Successfully started weave\n\n"; return 0; } \
+		|| { printf "\n\n\t Not able to start weave!!"; return 1; }
+	fi		 
 	}
 
+# check if docker is running, 'pgrep' returns 0, the process is running
+# DOCKER_SERVICE=docker
 
-
-#check if docker is running, 'pgrep' returns 0, the process is running
-DOCKER_SERVICE=docker
-if  (pgrep ${DOCKER_SERVICE} )
-  then
-
-	if [ -f $WEAVE_FILE ]; then
-	   echo "FILE : $WEAVE_FILE Exists, Proceeding to launch weave"
-	   launch_weave
-	   
-	else
-		echo ""
-	   echo "The file '$WEAVE_FILE' Does Not Exist. Downloading the images and booting them"
-	   curl -L git.io/weave -o /usr/local/bin/weave
-	   sudo chmod +x /usr/local/bin/weave
-	   launch_weave
-	fi
-
-  else
-	echo "${DOCKER_SERVICE} service is not running!"
+docker info > /dev/null 2>&1 || { printf "\n\tDocker is not running! Ensure Docker is running before running this script\n\n"; exit; }
+if [ -f $WEAVE_FILE ]; then
+   printf "\n\n\t FILE : $WEAVE_FILE Exists!!, \tProceeding to launch weave"
+   startWeave
+else
+   printf "\n\n\t The file '$WEAVE_FILE' Does Not Exist. Downloading the images and booting them"
+   curl -L git.io/weave -o /usr/local/bin/weave
+   sudo chmod +x /usr/local/bin/weave
+   startWeave
 fi
-
-
