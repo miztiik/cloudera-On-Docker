5ba0e0a7187be0594e6e497b493e9a0cb39e84ff
mystique <13oct08@quantumfoam.uni.me>
2015-08-23 18:54:54 +0530

    Updated to the latest docker version 1.8.1  - Added the reference article links  - Added error redirect for the iptables commands  - once again docker selinux installation to allow it to work in Centos7 to avoid a bug

---
+++ docker-host-build/setupDocker1.8.1.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# set -x
+##################################################################################
+## 
+## VERSION		:0.0.3
+## DATE			:24Aug2015
+##
+## USAGE		:This script enable docker 1.8.1 to use etc/sysconfig/ for additional options as they are not given by default
+## Ref[1]		:https://docs.docker.com/articles/configuring/
+## Ref[2]		:https://docs.docker.com/articles/systemd/
+## Ref[2]		:https://docs.docker.com/reference/commandline/daemon/
+##################################################################################
+
+systemctl stop docker
+
+mkdir /etc/systemd/system/docker.service.d
+
+rm -rf /etc/systemd/system/docker.service.d/docker-options.conf
+
+cat > /etc/systemd/system/docker.service.d/docker-options.conf << "EOF"
+
+[Service]
+EnvironmentFile=-/etc/sysconfig/docker
+EnvironmentFile=-/etc/sysconfig/docker-storage
+EnvironmentFile=-/etc/sysconfig/docker-network
+ExecStart=
+ExecStart=/usr/bin/docker -d -H fd:// $OPTIONS \
+      $DOCKER_STORAGE_OPTIONS \
+      $DOCKER_NETWORK_OPTIONS \
+      $BLOCK_REGISTRY \
+      $INSECURE_REGISTRY
+	  
+EOF
+
+# Setup docker selinux to allow it to setup bridge
+# https://github.com/docker/docker/issues/15498
+yum -y install docker-selinux
+
+# Set the temp directory & different location for images
+# printf 'OPTIONS="-g /media/sf_dockerRepos/dockerImages"\n' >> /etc/sysconfig/docker
+
+printf "DOCKER_TMPDIR=/media/sf_dockerRepos/dockerTmp\n" >> /etc/sysconfig/docker
+
+# Set the ip tables on host for weave to work with ICMP
+iptables -D INPUT -j REJECT --reject-with icmp-host-prohibited 2> /dev/null
+iptables -D FORWARD -j REJECT --reject-with icmp-host-prohibited 2> /dev/null
+
+# Reload configs and start docker
+systemctl daemon-reload
+systemctl start docker && \
+	{ 	
+		printf "\n\n\t ***************************************************"; \
+		printf "\n\n\t\t Successfully started docker"; \
+		printf "\n\n\t ***************************************************\n";
+	} \
+	|| printf "\n\n\t FAILED to start docker, check out 'systemctl status docker -l'\n\n"
+
