32fd999dae291363996fa9c2dd122adb3561ac87
mystique <13oct08@quantumfoam.uni.me>
2015-08-28 18:57:45 +0530

    Renamed the script to clouderaMgrNode-startup  - moved swapiness and service start here  - WIP, will try to finish the coming week
    Signed-off-by: mystique <13oct08@quantumfoam.uni.me>

---
+++ docker-image-build/hadoop/clouderaMgrNode/clouderaMgrNode-startup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -x
+##################################################################################
+## Cloudera Manager Node Start Script
+##
+## VERSION		:0.0.2
+## DATE			:28Aug2015
+##################################################################################
+
+
+# Probably will use them in the future
+DOCKER_IMAGE_NAME="local/hadoop_cloudera_mgr_base:v1"
+DOCKER_CONTAINERS_NAME="clouderamgrnode"             
+DOCKER_CONTAINERS_HOSTNAME="clouderamgrnode"         
+
+# start conteiner if "docker some_image run" earlier
+found_container=$(docker ps -a | grep "$DOCKER_CONTAINERS_NAME")
+if [ ! -n "$found_container" ]; then
+	
+	# Cloudera recommends swappiness to zero (throws error - moving to start up scripts)
+	# https://github.com/docker/docker/issues/5703
+	# https://github.com/docker/docker/issues/4717
+	sysctl vm.swappiness=0
+	
+	# Lets start the DB first followed by the manager server
+	service cloudera-scm-server-db start
+	sleep 30
+	service cloudera-scm-server start
+	
+	# RUN /etc/init.d/cloudera-scm-agent start
+
+	else
+		printf "\n\n\t\t The container $DOCKER_CONTAINERS_NAME is not running!!!\n\n"
+fi
