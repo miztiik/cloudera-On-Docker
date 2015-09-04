58add48232d1213c9de24833f5ad86d575743479
mystique <13oct08@quantumfoam.uni.me>
2015-09-04 19:51:36 +0530

    Moved the configuration stuff to  -  namenode1-setup.sh  -  datanode1-setup.sh  -  datanode2-setup.sh  -  datanode3-setup.sh
    Signed-off-by: mystique <13oct08@quantumfoam.uni.me>

--- docker-image-build/hadoop/hadoopClusterConf/hadoopClusterSetup.sh
+++ docker-image-build/hadoop/hadoopClusterConf/hadoopClusterSetup.sh
@@ -31,31 +31,3 @@ set -x
 # Yarn		:	8030 8031 8032 8033 8040 8042 8088
 # Other		:	49707 2122
 # ClouderaMgr:	7182	
-
-# Get user confirmation that we are running from Namenode
-read -p "You should be running this from namenode, Do you want to proceed? - " -n 1 -r
-echo ""
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    { printf "\n\n\tStarting to setup the configurations...Please wait"; }
-else
-	{ printf "\\n\n\t You chose to exit"\n\n; exit; }
-fi
-
-ssh -i /home/hadoopadmin/.ssh/id_rsa hadoopadmin@datanode1 
-
-# Format the namenode
-sudo -u hdfs hdfs namenode -format
-
-
-
-# To start the HDFS on each node
-for x in `cd /etc/init.d ; ls hadoop-hdfs-*` ; do sudo service $x start ; done
-
-# Hadoop needs a tmp directory with the right permissions
-sudo -u hdfs hadoop fs -mkdir /tmp
-sudo -u hdfs hadoop fs -chmod -R 1777 /tmp
-
-##################################################
-#				END OF INSTALLATION				 #
-##################################################
