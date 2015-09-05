27eb7f27de9c7bc62a2098ed933f5485c7a17220
mystique <13oct08@quantumfoam.uni.me>
2015-09-06 00:03:11 +0530

    Ccosmetic changes to correct the indentdation of the ports
    Signed-off-by: mystique <13oct08@quantumfoam.uni.me>

--- docker-image-build/hadoop/hadoopClusterConf/datanode1-setup.sh
+++ docker-image-build/hadoop/hadoopClusterConf/datanode1-setup.sh
@@ -26,11 +26,11 @@
 #################################################
 
 # Expose the relevant ports
-# HDFS		:	8020 50010 50020 50070 50075 50090
-# Mapred	:	19888
-# Yarn		:	8030 8031 8032 8033 8040 8042 8088
-# Other		:	49707 2122
-# ClouderaMgr:	7182
+# HDFS			:	8020 50010 50020 50070 50075 50090
+# Mapred		:	19888
+# Yarn			:	8030 8031 8032 8033 8040 8042 8088
+# Other			:	49707 2122
+# ClouderaMgr	:	7182
 
 [[ "$(hostname -s)" = "datanode1" ]] && { printf "\n\n\t Procceding with configuring the "$(hostname -s)" ...\n\n"; } || { printf "\n\n\t You are on the wrong node - "$(hostname -s)"\n\n"; exit;}
 
